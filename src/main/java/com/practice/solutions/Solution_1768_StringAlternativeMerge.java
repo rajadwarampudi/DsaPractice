be0cca9164971cbd983fc8de65ded2e636dd8ce5
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-01-09 00:13:13 +0100

    solve problem 1768 merge two strings alternatively

---
+++ src/main/java/com/practice/solutions/Solution_1768_StringAlternativeMerge.java
@@ -0,0 +1,28 @@
+package com.practice.solutions;
+
+public class Solution_1768_StringAlternativeMerge {
+
+    /**
+     * Merge two strings by alternative characters from each. append the leftovers at the end.
+     * @param word1
+     * @param word2
+     * @return  a string the is made of merging word1 and word2's alternative characters
+     */
+
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i < word1.length() && i < word2.length()) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+            i++;
+        }
+
+        if (i == word1.length()) {
+            sb.append(word2, i, word2.length());
+        } else {
+            sb.append(word1, i, word1.length());
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/practice/solutions/TestSolution1768StringAlternativeMerge.java
@@ -0,0 +1,17 @@
+package com.practice.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution1768StringAlternativeMerge {
+
+    @Test
+    public void testSolution_1768_StringAlternativeMerge() {
+        Solution_1768_StringAlternativeMerge solution = new Solution_1768_StringAlternativeMerge();
+
+        assertEquals(solution.mergeAlternately("abc", "def"), "adbecf");
+        assertEquals(solution.mergeAlternately("pq", "rstu"), "prqstu");
+        assertEquals(solution.mergeAlternately("rstu", "pq"), "rpsqtu");
+    }
+}
