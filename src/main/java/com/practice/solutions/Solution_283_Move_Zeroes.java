62cf6a173120b1afe9f03af29a17bbbeeb903598
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-01-11 00:37:04 +0100

    Solve the problem 283 Move Zeroes

---
+++ src/main/java/com/practice/solutions/Solution_283_Move_Zeroes.java
@@ -0,0 +1,20 @@
+package com.practice.solutions;
+
+public class Solution_283_Move_Zeroes {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 1;
+
+        while (j < nums.length) {
+            if (nums[i] != 0) {
+                i++;
+            } else {
+                if (nums[j] != 0) {
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                    i++;
+                }
+            }
+            j++;
+        }
+    }
+}
---
+++ src/test/java/com/practice/solutions/TestSolution283MoveZeroes.java
@@ -0,0 +1,22 @@
+package com.practice.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class TestSolution283MoveZeroes {
+
+    @Test
+    public void testMoveZeroes() {
+        Solution_283_Move_Zeroes solution = new Solution_283_Move_Zeroes();
+
+        int[] nums = new int[]{0,1,0,3,12};
+        solution.moveZeroes(nums);
+        assertArrayEquals(new int[]{1,3,12,0,0}, nums);
+
+        int[] numsScenario2 = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0};
+        solution.moveZeroes(numsScenario2);
+        assertArrayEquals(new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, numsScenario2);
+
+    }
+}
