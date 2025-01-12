e902fea7aa67ff3844e58981a45d454bc30d6e01
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-01-12 23:59:43 +0100

    Solve problem 11 Container with most water

---
+++ src/main/java/com/practice/solutions/Solution_11_ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package com.practice.solutions;
+
+public class Solution_11_ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int leftIndex = 0, rightIndex = height.length - 1;
+
+        int maxArea = 0;
+        while (leftIndex < rightIndex) {
+            int width = rightIndex - leftIndex;
+            int depth = Math.min(height[leftIndex], height[rightIndex]);
+            int tempMaxArea = depth * width;
+            maxArea = Math.max(tempMaxArea, maxArea);
+            if (depth == height[leftIndex]) {
+                leftIndex++;
+            } else {
+                rightIndex--;
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/test/java/com/practice/solutions/TestSolution11ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+package com.practice.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution11ContainerWithMostWater {
+
+    @Test
+    public void testReverseWords() {
+        Solution_11_ContainerWithMostWater solution = new Solution_11_ContainerWithMostWater();
+
+        assertEquals(49, solution.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+        assertEquals(1, solution.maxArea(new int[]{1,1}));
+        assertEquals(189, solution.maxArea(new int[]{9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,10}));
+        assertEquals(210, solution.maxArea(new int[]{10,9,9,9,9,9,9,9,9,9,20,9,9,9,9,9,9,9,9,9,9,10}));
+
+    }
+}
