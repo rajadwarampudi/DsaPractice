01363053a7b805a493866c47ccebb90be886ec9e
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-01-13 22:16:11 +0100

    Solve and test Problem 1679 MaxKSumPairs

---
+++ src/main/java/com/practice/solutions/Solution_1679_MaxKSumPairs.java
@@ -0,0 +1,26 @@
+package com.practice.solutions;
+
+import java.util.Arrays;
+
+public class Solution_1679_MaxKSumPairs {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int i = 0, j = nums.length - 1;
+        int operationCount = 0;
+
+        while (i < j) {
+            int tempSum = nums[i] + nums[j];
+            if(tempSum == k) {
+                operationCount++;
+                i++;
+                j--;
+            } else if (tempSum > k) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return operationCount;
+    }
+}
---
+++ src/test/java/com/practice/solutions/TestSolution1679MaxKSumPairs.java
@@ -0,0 +1,17 @@
+package com.practice.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution1679MaxKSumPairs {
+    @Test
+    public void testMaxOperations() {
+        Solution_1679_MaxKSumPairs solution = new Solution_1679_MaxKSumPairs();
+
+        assertEquals(2, solution.maxOperations(new int[]{1,2,3,4}, 5));
+        assertEquals(0, solution.maxOperations(new int[]{1}, 5));
+        assertEquals(13, solution.maxOperations(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, 2));
+        assertEquals(1, solution.maxOperations(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1}, 3));
+    }
+}
