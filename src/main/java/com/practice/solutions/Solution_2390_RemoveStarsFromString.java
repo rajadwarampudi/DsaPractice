db93fef31db472cf8fa51d7afea0db03b2447033
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-01-15 22:45:49 +0100

    Solve Problem 2390 RemoveStarsFromString

---
+++ src/main/java/com/practice/solutions/Solution_2390_RemoveStarsFromString.java
@@ -0,0 +1,27 @@
+package com.practice.solutions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution_2390_RemoveStarsFromString {
+    public String removeStars(String s) {
+        Deque<Character> charsStack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) {
+            if(ch == '*') {
+                if (charsStack.peek() != null) {
+                    charsStack.pop();
+                }
+            } else {
+                charsStack.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (charsStack.peekLast() != null) {
+            sb.append(charsStack.pollLast());
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/practice/solutions/TestSolution2390RemoveStarsFromString.java
@@ -0,0 +1,16 @@
+package com.practice.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution2390RemoveStarsFromString {
+
+    @Test
+    public void testReverseWords() {
+        Solution_2390_RemoveStarsFromString solution = new Solution_2390_RemoveStarsFromString();
+
+        assertEquals("lecoe", solution.removeStars("leet**cod*e"));
+        assertEquals("", solution.removeStars("leet****"));
+    }
+}
